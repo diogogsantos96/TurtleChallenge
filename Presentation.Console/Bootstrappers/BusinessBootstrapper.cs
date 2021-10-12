c9fc185df397a0e2ff9d3232b4d4dd83ecf32701
Diogo Santos <diogogmsantos96@gmail.com>
2021-10-13 00:03:23 +0100

    fix: added possibility for dependency injection

---
+++ Presentation.Console/Bootstrappers/BusinessBootstrapper.cs
@@ -0,0 +1,11 @@
+﻿namespace Presentation.Console.Bootstrappers
+{
+    using Business.Services.Factories;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class BusinessBootstrapper
+    {
+        public static IServiceCollection AddBusinessServices(this IServiceCollection @this)
+            => @this.AddScoped<ITurtleChallengeAlgorithmFactory, TurtleChallengeAlgorithmFactory>();
+    }
+}
--- Presentation.Console/Program.cs
+++ Presentation.Console/Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace TurtleChallenge
 {
     using Business.Services.Factories;
-
+    using Microsoft.Extensions.DependencyInjection;
     using Model.Domain;
     using Model.Domain.Parsers;
     using Newtonsoft.Json;
+    using Presentation.Console.Bootstrappers;
     using Presentation.Console.Output;
 
     using System;
@@ -15,12 +16,18 @@
     {
         static void Main(string[] args)
         {
+            var serviceProvider = new ServiceCollection()
+                .AddBusinessServices()
+                .BuildServiceProvider();
+
             try
             {
                 var gameSettings = new JsonReader<GameSettings>().Read(args[0]);
                 var moves = new JsonReader<IEnumerable<string>>().Read(args[1]);
 
-                var service = new TurtleChallengeAlgorithmFactory().GetAlgorithm();
+                var service = serviceProvider.GetRequiredService<ITurtleChallengeAlgorithmFactory>()
+                    .GetAlgorithm();
+
                 int index = 1;
 
                 foreach(var sequence in moves)
